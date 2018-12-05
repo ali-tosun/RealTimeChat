2d65639301eae4eb76c69a40a6a624613b95e812
ali-tosun <a.tosun.420@gmail.com>
2018-12-05 17:17:25 +0300

    Firebase Cloud Messaging dependecy leri eklendi ve Firebase core sürümü düşürüldü.

---
+++ app/src/main/java/com/tosun/ali/sohbetodam/Services/MyFirebaseMessagingService.kt
@@ -0,0 +1,24 @@
+package com.tosun.ali.sohbetodam.Services
+
+import android.util.Log
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+
+
+
+    override fun onMessageReceived(p0: RemoteMessage?) {
+
+
+        var bildirimBaslik = p0?.notification?.title
+
+        var bildirimBody = p0?.notification?.body
+
+        var data = p0?.data
+
+        Log.e("FCM","bildirim baslik $bildirimBaslik bildirim body \n $bildirimBody \n data $data")
+
+
+    }
+}
